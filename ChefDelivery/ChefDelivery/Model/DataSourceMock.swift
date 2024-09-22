5706869ad2e8f0d1c07ef9deed636aaab12c1718
Livya Gomes <livyagomesdc@gmail.com>
2024-09-22 02:00:01 -0300

    Assets, order types e mock

---
+++ ChefDelivery/ChefDelivery/Model/DataSourceMock.swift
@@ -0,0 +1,18 @@
+//
+//  DataSourceMock.swift
+//  ChefDelivery
+//
+//  Created by lgc on 22/09/24.
+//
+
+import Foundation
+
+let ordersMock: [OrderType] = [
+    OrderType(id: 1, name: "Restaurante", image: "hamburguer"),
+    OrderType(id: 2, name: "Mercado", image: "mercado"),
+    OrderType(id: 3, name: "Farmácia", image: "farmacia"),
+    OrderType(id: 4, name: "Descontos", image: "descontos"),
+    OrderType(id: 5, name: "Gourmet", image: "gourmet"),
+    OrderType(id: 6, name: "Pet", image: "petshop"),
+    OrderType(id: 7, name: "Bebidas", image: "bebidas")
+]
---
+++ ChefDelivery/ChefDelivery/Model/OrderType.swift
@@ -0,0 +1,14 @@
+//
+//  OrderType.swift
+//  ChefDelivery
+//
+//  Created by lgc on 22/09/24.
+//
+
+import Foundation
+
+struct OrderType: Identifiable {
+    let id: Int
+    let name: String
+    let image: String
+}
