0e09890c635dbb0ad9f6ad4b2f86931333d9294b
Livya Gomes <livyagomesdc@gmail.com>
2024-10-01 16:27:10 -0300

    State

---
+++ ChefDelivery/ChefDelivery/ProductView/ProductDetailView.swift
@@ -0,0 +1,92 @@
+//
+//  ProductDetailView.swift
+//  ChefDelivery
+//
+//  Created by lgc on 01/10/24.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    let product: ProductType
+    @State private var productQuantity = 1
+    
+    var body: some View {
+        VStack {
+            VStack(alignment: .leading, spacing: 16) {
+                Image(product.image)
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(radius: 20)
+                
+                Text(product.name)
+                    .font(.title)
+                    .bold()
+                    .padding(.horizontal)
+                
+                Text(product.description)
+                    .padding(.horizontal)
+                
+                Text(product.formattedPrice)
+                    .font(.title3)
+                    .bold()
+                    .padding(.horizontal)
+            }
+            
+            Spacer()
+            
+            VStack {
+                Text("Quantidade")
+                    .font(.title3)
+                    .bold()
+                
+                HStack {
+                    Button{
+                        if productQuantity > 1 {
+                            productQuantity -= 1
+                        }
+                    } label: {
+                        Image(systemName: "minus.circle.fill")
+                            .font(.title)
+                            .bold()
+                    }
+                    
+                    Text("\(productQuantity)")
+                    
+                    Button{
+                        productQuantity += 1
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .font(.title)
+                            .bold()
+                    }
+                }
+            }
+            
+            Spacer()
+            
+            Button {
+                print("Adicionar ao carrinho")
+                
+            } label: {
+                HStack {
+                    Image(systemName: "cart")
+                    
+                    Text("Adicionar ao carrinho")
+                }
+                .padding(.horizontal, 32)
+                .padding(.vertical, 16)
+                .font(.title3)
+                .bold()
+                .background(Color("ColorRed"))
+                .foregroundColor(.white)
+                .cornerRadius(32)
+                .shadow(color: Color("ColorRedDark").opacity(0.5), radius: 10, x: 6, y: 8)
+            }
+        }
+    }
+}
+
+#Preview {
+    ProductDetailView(product: storesMock[0].products[0])
+}
--- ChefDelivery/ChefDelivery/StoresView/StoreDetailView.swift
+++ ChefDelivery/ChefDelivery/StoresView/StoreDetailView.swift
@@ -45,30 +45,37 @@ struct StoreDetailView: View {
                     .font(.title)
                     .bold()
                     .padding()
-                
+
                 ForEach(store.products) { product in
-                    HStack(spacing: 8) {
-                        VStack(alignment: .leading, spacing: 8) {
-                            Text(product.name)
-                                .fontWeight(.bold)
+                    
+                    NavigationLink {
+                        ProductDetailView(product: product)
+                    } label: {
+                        HStack(spacing: 8) {
+                            VStack(alignment: .leading, spacing: 8) {
+                                Text(product.name)
+                                    .fontWeight(.bold)
+                                
+                                Text(product.description)
+                                    .foregroundColor(.black.opacity(0.5))
+                                    .multilineTextAlignment(.leading)
+                                
+                                Text(product.formattedPrice)
+                            }
                             
-                            Text(product.description)
-                                .foregroundColor(.black.opacity(0.5))
+                            Spacer()
                             
-                            Text(product.formattedPrice)
+                            Image(product.image)
+                                .resizable()
+                                .scaledToFit()
+                                .cornerRadius(12)
+                                .frame(width: 120, height: 120)
+                                .shadow(color: .black.opacity(1), radius: 20, x: 6, y: 8)
+                            
                         }
-                        
-                        Spacer()
-                        
-                        Image(product.image)
-                            .resizable()
-                            .scaledToFit()
-                            .cornerRadius(12)
-                            .frame(width: 120, height: 120)
-                            .shadow(color: .black.opacity(1), radius: 20, x: 6, y: 8)
-            
+                        .padding()
+                        .foregroundColor(.black)
                     }
-                    .padding()
                 }
             }
             .navigationTitle(store.name)
@@ -80,7 +87,7 @@ struct StoreDetailView: View {
                         presentationMode.wrappedValue.dismiss()
                     } label: {
                         HStack(spacing: 4) {
-                            Image(systemName: "cart")
+                            Image(systemName: "building.2")
                             
                             Text("Lojas")
                         }
