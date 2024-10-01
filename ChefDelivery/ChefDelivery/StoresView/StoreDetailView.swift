2f1453796b279fd111c1e845ae3ee4d34c95dd4b
Livya Gomes <livyagomesdc@gmail.com>
2024-10-01 15:23:14 -0300

    Criando a tela de restaurantes

--- ChefDelivery/ChefDelivery/StoresView/StoreDetailView.swift
+++ ChefDelivery/ChefDelivery/StoresView/StoreDetailView.swift
@@ -9,9 +9,86 @@ import SwiftUI
 
 struct StoreDetailView: View {
     let store: StoreType
+    @Environment(\.presentationMode)var presentationMode
     
     var body: some View {
-        Text(store.name)
+        ScrollView(showsIndicators: false) {
+            VStack(alignment: .leading) {
+                Image(store.headerImage)
+                    .resizable()
+                    .scaledToFit()
+                
+                HStack {
+                    Text(store.name)
+                        .font(.title)
+                        .bold()
+                    Spacer()
+                    
+                    Image(store.logoImage)
+                }
+                .padding(.vertical, 8)
+                .padding(.horizontal)
+                
+                HStack {
+                    Text(store.location)
+                    
+                    ForEach(1...store.stars, id: \.self) { _ in
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.yellow)
+                            .font(.caption)
+                    }
+                }
+                .padding(.vertical, 8)
+                .padding(.horizontal)
+                
+                Text("Produtos")
+                    .font(.title)
+                    .bold()
+                    .padding()
+                
+                ForEach(store.products) { product in
+                    HStack(spacing: 8) {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text(product.name)
+                                .fontWeight(.bold)
+                            
+                            Text(product.description)
+                                .foregroundColor(.black.opacity(0.5))
+                            
+                            Text(product.formattedPrice)
+                        }
+                        
+                        Spacer()
+                        
+                        Image(product.image)
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(12)
+                            .frame(width: 120, height: 120)
+                            .shadow(color: .black.opacity(1), radius: 20, x: 6, y: 8)
+            
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle(store.name)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        HStack(spacing: 4) {
+                            Image(systemName: "cart")
+                            
+                            Text("Lojas")
+                        }
+                        .foregroundColor(Color("ColorRed"))
+                    }
+                }
+            }
+        }
     }
 }
 
