5458f5839b22c066e283da20a4ebbfd6f2f63813
Livya Gomes <livyagomesdc@gmail.com>
2024-10-08 14:11:25 -0300

    Requisição HTTP com apiary

--- ChefDelivery/ChefDelivery/App/ContentView.swift
+++ ChefDelivery/ChefDelivery/App/ContentView.swift
@@ -23,6 +23,26 @@ struct ContentView: View {
                 }
             }
         }
+        .onAppear{fetchData()}
+    }
+    
+    func fetchData() {
+        guard let url = URL(string: "https://private-aaf161-livyagomes.apiary-mock.com/questions") else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) {data, _, error in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data) as? [[String: Any]]
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+        }.resume()
     }
 }
 
