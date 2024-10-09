5cc35f46c9791d89e0cefcd1bef2ae825c6c9373
Livya Gomes <livyagomesdc@gmail.com>
2024-10-09 16:46:21 -0300

    Verbo POST

--- ChefDelivery/ChefDelivery/Networking/HomeService.swift
+++ ChefDelivery/ChefDelivery/Networking/HomeService.swift
@@ -25,6 +25,24 @@ struct HomeService {
         
         return .success(storesObjects)
     }
+    
+    func confirmOrder(product: ProductType) async throws -> Result<[String: Any]?, RequestError> {
+        guard let url = URL(string: "https://private-aaf161-livyagomes.apiary-mock.com/home") else {
+            return .failure(.invalidURL)
+        }
+        
+        let encodedObject = try JSONEncoder().encode(product)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = encodedObject
+        
+        let(data, _) = try await URLSession.shared.data(for: request)
+        let message = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+        
+        return .success(message)
+        
+    }
 }
 
 
--- ChefDelivery/ChefDelivery/ProductView/ProductDetailView.swift
+++ ChefDelivery/ChefDelivery/ProductView/ProductDetailView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ProductDetailView: View {
+    
+    var service = HomeService()
     let product: ProductType
     @State private var productQuantity = 1
     
@@ -21,9 +23,54 @@ struct ProductDetailView: View {
         
         Spacer()
         
-        ProductDetailButtonView()
+        ProductDetailButtonView{
+            Task {
+                await confirmOrder()
+            }
+        }
+    }
+    
+    func confirmOrder() async {
+        do {
+            let result = try await service.confirmOrder(product: product)
+            switch result {
+            case .success(let message):
+                print(message)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
+
+struct ProductDetailButtonView: View {
+    
+    var onButtonPress: () -> Void
+    
+    var body: some View {
+        Button {
+            onButtonPress()
+            
+        } label: {
+            HStack {
+                Image(systemName: "cart")
+                
+                Text("Adicionar ao carrinho")
+            }
+            .padding(.horizontal, 32)
+            .padding(.vertical, 16)
+            .font(.title3)
+            .bold()
+            .background(Color("ColorRed"))
+            .foregroundColor(.white)
+            .cornerRadius(32)
+            .shadow(color: Color("ColorRedDark").opacity(0.5), radius: 10, x: 6, y: 8)
+        }
+    }
+}
+
 
 #Preview {
     ProductDetailView(product: storesMock[0].products[0])
