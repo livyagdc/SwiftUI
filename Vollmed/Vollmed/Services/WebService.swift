01098f15cbb0296edd04bd1919ac57d068ecffe3
Livya Gomes <livyagomesdc@gmail.com>
2024-10-15 17:44:21 -0300

    Requisição GET e async/await

--- Vollmed/Vollmed/Services/WebService.swift
+++ Vollmed/Vollmed/Services/WebService.swift
@@ -6,12 +6,48 @@
 //
 
 import Foundation
+import UIKit
 
 struct WebService {
     
     private let baseURL = "http://localhost:3000"
+    let imageCache = NSCache<NSString, UIImage>()
     
-    func getAllSpecialists() {
+    func downloadImage(from imageURL: String) async throws -> UIImage? {
+        guard let url = URL(string: imageURL) else {
+            print("Erro na URL!")
+            return nil
+        }
+        
+        //Verificar cache
+        if let cachedImage = imageCache.object(forKey: imageURL as NSString) {
+            return cachedImage
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        guard let image = UIImage(data: data) else {
+            return nil
+        }
+        
+        imageCache.setObject(image, forKey: imageURL as NSString)
+        
+//        return UIImage(data: data)
+        return image
+    }
+    
+    func getAllSpecialists() async throws -> [Specialist]? {
+        let endpoint = baseURL + "/especialista"
+        
+        guard let url = URL(string: endpoint) else {
+            print("Erro na URL!")
+            return nil
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        let specialists = try JSONDecoder().decode([Specialist].self, from: data)
+        
+        return specialists
         
     }
 }
