942f5e2c07854531cf5db1e15d00d941de4f43df
Livya Gomes <livyagomesdc@gmail.com>
2024-10-18 15:44:40 -0300

    Criação HTTPClient

---
+++ MVVM/Vollmed/Vollmed/Networking/Endpoints/HomeEndpoint.swift
@@ -0,0 +1,43 @@
+//
+//  HomeEndpoint.swift
+//  Vollmed
+//
+//  Created by lgc on 18/10/24.
+//
+
+import Foundation
+
+enum HomeEndpoint {
+    case getAllSpecialists
+}
+
+extension HomeEndpoint: Endpoint {
+    var path: String {
+        switch self {
+        case .getAllSpecialists:
+            return "/especialista"
+        }
+    }
+    
+    var method: RequestMethod {
+        switch self {
+        case .getAllSpecialists:
+            return .get
+        }
+    }
+    
+    var header: [String : String]? {
+        switch self {
+        case .getAllSpecialists:
+            return nil
+        }
+    }
+    
+    var body: [String : String]? {
+        switch self {
+        case .getAllSpecialists:
+            return nil
+        }
+    }
+    
+}
